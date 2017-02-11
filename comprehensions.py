bf5434048320079d73c6ed0134686fabf3a3d61f
Gleb <gleb.m.sokolov@gmail.com>
2017-02-11 17:12:55 -0500

    this is an important thing!

---
+++ comprehensions.py
@@ -0,0 +1,24 @@
+# comprehensions in dict
+
+names = ['clark', 'bruce', 'peter', 'wade']
+identity = ['superman', 'batman', 'spiderman', 'deadpool']
+
+reveal = {name: hero for name, hero in zip(names, identity)}
+# print(reveal)
+
+# generators
+
+li = [n + 1 for n in range(10)]
+print(li)
+
+#
+# def gen_foo(nums):
+#     for i in nums:
+#         yield i * i
+#
+# my_gen = gen_foo(li)
+
+my_gen = (i*i for i in li )
+
+for i in my_gen:
+    print i
