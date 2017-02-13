e5997be25167d4c6968c12f126ee11d11c1c2d06
Gleb <gleb.m.sokolov@gmail.com>
2017-02-12 19:05:32 -0500

    sys.argv array and std streams

---
+++ modulesys.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.stderr.write('test\n')
+sys.stderr.flush()
+sys.stdout.write('another test\n')
+
+print sys.argv
+#sys argv is array of arg in term
+#sys.argv[0] len(sys.argv)
+
+
