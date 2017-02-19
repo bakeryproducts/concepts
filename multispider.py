29c7d1188008bc1129069d2234e93352f4cdfa84
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-02-19 14:56:30 +0000

    webcrawler with multiprocessing

--- multispider.py
+++ multispider.py
@@ -16,31 +16,67 @@ def handle_local(url, link):
     return urljoin(url, link)
 
 
+def exlist(link):                                       #this is probably bad idea
+    list = ['mailto:', 'tel:', 'javascript:']
+    for el in list:
+        if el in link:
+            return True
+    return False
+
+
 def get_links(url):
     try:
         resp = requests.get(url)
         soup = bs.BeautifulSoup(resp.text, 'lxml')
         body = soup.body
-        links = [link.get('href') for link in body.find_all('a')]
+        links = [link.get('href') for link in body.find_all('a', href=True) if not exlist(link.get('href').lower())]
         links = [handle_local(url, link) for link in links]
-        links = [str(link.encode('ascii')) for link in links]
+        # links = [str(link.encode('utf-8')) for link in links]
         return links
     except Exception as e:
         print(e)
         return []
 
 
+def check_dom(url, visited):
+    dom = url.split('//')[-1].split('/')[0]
+    if dom not in visited:
+        visited.append(dom)
+        return True
+    else:
+        return False
+
+
 def main():
-    count = 3
+    count = 1
     p = Pool(processes=count)
     parse_us = [url_gen() for _ in range(count)]
-    data = p.map(get_links, parse_us)
-    p.close()
+    print(parse_us)
+    visited_domain = []
+    while 'www.youtube.com' not in visited_domain:
+        data = p.map(get_links, parse_us)
+        data = [url for site in data for url in site]
+        data_out = '\n'.join(data)
+        with open('testingurl.txt', 'w') as f:
+            f.write(data_out)
 
-    data = [url for site in data for url in site]
-    data = '\n'.join(data)
-    with open('testingurl.txt', 'w') as f:
-        f.write(data)
+        clean_data = []                                         # and this clear_data thing is bad too
+        for ind, url in enumerate(data):
+            if check_dom(url, visited_domain):
+                clean_data.append(url)
+        data = clean_data
+
+        parse_us = data
+
+        print(parse_us)
+        count += 1
+        if count > 10 or len(parse_us) == 0:
+            print('quiting, hit top!')
+            break
+
+    with open('visited.txt', 'w') as f:
+        f.write('\n'.join(visited_domain))
+    p.close()
 
 
 if __name__ == '__main__':
