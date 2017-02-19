94d4f79aab9481de3ae5d568bddcefc57ead08e0
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-02-19 16:07:19 +0000

    webcrawler with multiprocessing

--- multispider.py
+++ multispider.py
@@ -8,15 +8,11 @@ import string
 
 def url_gen():
     st = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(3))
-    st = ''.join(['http://', st, '.com'])
+    st = ''.join(['http://', st, '.ru'])
     return st
 
 
-def handle_local(url, link):
-    return urljoin(url, link)
-
-
-def exlist(link):                                       #this is probably bad idea
+def exlist(link):  # this is probably bad idea
     list = ['mailto:', 'tel:', 'javascript:']
     for el in list:
         if el in link:
@@ -30,7 +26,7 @@ def get_links(url):
         soup = bs.BeautifulSoup(resp.text, 'lxml')
         body = soup.body
         links = [link.get('href') for link in body.find_all('a', href=True) if not exlist(link.get('href').lower())]
-        links = [handle_local(url, link) for link in links]
+        links = [urljoin(url, link) for link in links]
         # links = [str(link.encode('utf-8')) for link in links]
         return links
     except Exception as e:
@@ -38,41 +34,35 @@ def get_links(url):
         return []
 
 
-def check_dom(url, visited):
+def get_dom(url):
     dom = url.split('//')[-1].split('/')[0]
-    if dom not in visited:
-        visited.append(dom)
-        return True
-    else:
-        return False
+    return dom
 
 
 def main():
     count = 1
-    p = Pool(processes=count)
+    p = Pool(processes=2)
     parse_us = [url_gen() for _ in range(count)]
-    print(parse_us)
     visited_domain = []
-    while 'www.youtube.com' not in visited_domain:
+    visited_urls = []
+    print(parse_us)
+    while 'www.youtube.com' not in visited_domain and len(parse_us) > 0 and count < 10:
         data = p.map(get_links, parse_us)
         data = [url for site in data for url in site]
-        data_out = '\n'.join(data)
+        data = list(set(data))
         with open('testingurl.txt', 'w') as f:
-            f.write(data_out)
+            f.write('\n'.join(data))
 
-        clean_data = []                                         # and this clear_data thing is bad too
-        for ind, url in enumerate(data):
-            if check_dom(url, visited_domain):
-                clean_data.append(url)
-        data = clean_data
+        parse_us = [url for url in data if url not in visited_urls]
+        visited_urls.extend(parse_us)
+        visited_domain.extend([get_dom(url) for url in parse_us if get_dom(url) not in visited_domain])
+        if len(parse_us)>100:
+            parse_us=parse_us[:100]
 
-        parse_us = data
 
-        print(parse_us)
+        print(len(data), ' urls in data; new ones: ', parse_us)
+
         count += 1
-        if count > 10 or len(parse_us) == 0:
-            print('quiting, hit top!')
-            break
 
     with open('visited.txt', 'w') as f:
         f.write('\n'.join(visited_domain))
