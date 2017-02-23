8b181a5d5d779d6e2aeab71a010fc414102479d0
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-02-23 15:21:53 +0000

    webcrawler with multiprocessing

---
+++ tupl.py
@@ -0,0 +1,13 @@
+# namedtuple
+from collections import namedtuple
+
+Person = namedtuple('Person',['name','age'])
+
+p = Person('George','24')
+
+print(p.age)
+print(p.name)
+
+print(p._asdict())
+p2 = p._replace(name='bill')
+print(p,p2)
