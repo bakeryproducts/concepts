68c0d4176c4ccd18ac9d78a98cfe74632464d2db
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-02-27 22:24:35 +0000

    apmonitor hw8 (numpy)

--- apmonitor_hw8.py
+++ apmonitor_hw8.py
@@ -1,25 +1,51 @@
+#numpy basic matrix operations
+
 import numpy as np
 import random
 
-x = np.ones(50)
+#x = np.ones(50)
 
 y = np.linspace(1, 20, 50)
-z=np.zeros(50)
+z = np.zeros(50)
 
 for i in range(50):
-    z[i] = (i) * 19/49+1
+    z[i] = (i) * 19 / 49 + 1
 
-
-print(z)
+# print(z)
 print()
-print(np.sum(abs(z-y)))
+# print(np.sum(abs(z-y)))
 
-m=np.ones(1)
-#m.shape=(1,1)
-i=0
-while m[-1] != 7 and i<=30:
-    m = np.append(m, random.randrange(1,10))
-    i+=1
+m = np.ones(30)
+# m.shape=(1,1)
+for i in range(30):
+    m[i] = random.randrange(10)
 
 print(m)
 print(m.mean())
+
+a = np.random.randint(1, 10, size=30)
+
+a = a.reshape([5, 6])
+
+for i in np.arange(np.size(a, 0)):
+    for j in np.arange(np.size(a, 1)):
+        if a[i, j] == 5:
+            print(i, j)
+
+# print(a)
+
+
+a = np.arange(1, 9 + 1).reshape((3,3))
+b=np.array([[-.1,-.2,-.3]
+           ,[3,10,2],
+            [4,2,.5]])
+x = np.array([[1, 3, 4]])           # do double [[ for later transposing
+x=x.T                               # like that
+
+print(np.dot(a,b))                  # two variants (works frequently for many operations)
+print(a.dot(b))
+print(np.cross(a,b))                # x product
+print(a.dot(x))
+
+np.linalg.inv(a)                    # from lin algebra :inverse A, A^-1
+
