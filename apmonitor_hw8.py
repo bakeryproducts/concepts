cdf0b26be09428773e44a364c6b8029cb14f6059
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-02-27 23:52:33 +0000

    apmonitor hw8 (numpy)

--- apmonitor_hw8.py
+++ apmonitor_hw8.py
@@ -49,3 +49,35 @@ print(a.dot(x))
 
 np.linalg.inv(a)                    # from lin algebra :inverse A, A^-1
 
+
+a = np.array([1,2,3,4]).reshape(2,2)
+b=a
+b[0]=13
+c=[1,2,3,4]
+d=c
+d[0]=13
+
+print(c)
+
+a = np.zeros((2,2,3))
+print(a)
+a=np.ravel(a)                       # flatten
+print(a)
+print('\n'*10)
+twocubes = np.random.randint(1,10,size=8)
+subscript = [7,5]                   # can use for a[subscript]
+print(twocubes)
+#print(twocubes[subscript])
+twocubes=twocubes.reshape(2,2,2)
+print(twocubes[:,:,0])                     # : means all, i.e. a.shape = 2,2,2
+                                    # a[:,:,0] means select all dimensions on level 0
+                                    # -> 2x2 matrix  then select all 1D  from them : 1x2
+                                    # strings , then look for 0 element
+
+a = np.ones(9,dtype=int).reshape(3,3)
+a[1,0]=5
+print(a)
+print(np.mean(a,0))                 # meam(a,x) gives mean values in axes:
+                                    # 0 is 'down' 1 is 'right'
+print(twocubes)
+print(np.mean(twocubes,2))          # for 3D 0 is 'depth', 1 is 'down' 2 is 'right'
