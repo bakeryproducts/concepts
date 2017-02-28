4a229d6df2eba3c72e5053a9fbac53c968aa8f1b
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-02-28 19:13:29 +0000

    collection deque

--- functls.py
+++ functls.py
@@ -3,7 +3,17 @@ import functools
 #@functools.lru_cache(maxsize=64)       # memoized for naturalov
 
 f = functools.partial(sorted,key=lambda p: p[1]) # fix arg #2
-print(f([('a',2),('b',1)]))
+#print(f([('a',2),('b',1)]))
 
+from collections import deque
+import numpy
+x=range(10)
+
+
+a = deque(x,maxlen=4)
+a.extend([1,1])
+a.popleft()
+a.extendleft([8,8])
+print(a)
 
 
