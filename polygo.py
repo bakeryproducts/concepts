984a974279da92431f8ef2ce2307a62494fd741b
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-03-07 22:58:13 +0000

    polygons

--- polygo.py
+++ polygo.py
@@ -1,23 +1,32 @@
+# Polygon from matplotlib and Area
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 
-pat = []
-n=3
+def PolyArea(x,y):
+    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
-for i in range(n):
+pat = []
+n=5
+
+for i in range(2):
     polygon = Polygon(np.random.rand(n,2),True)
     pat.append(polygon)
 
-p = PatchCollection(pat,alpha=.4)
+p = PatchCollection(pat,alpha=.8)
 colors = 100*np.random.rand(len(pat))
 p.set_array(np.array(colors))
 
 fig,ax = plt.subplots()
 ax.add_collection(p)
-pp=np.array(pat[0].get_xy())
-print(pp[0])
+
+for poly in pat:
+    pp=np.array(poly.get_xy())
+
+    print(PolyArea(pp[:,0],pp[:,1]))
+
 
 plt.show()
