1b96c27aa699dac3d5f79ea8791b2c22a2687ea4
bakeryproducts <gleb.m.sokolov@gmail.com>
2017-03-08 15:40:36 +0000

    polygons

--- polygo.py
+++ polygo.py
@@ -11,19 +11,34 @@ def PolyArea(x, y):
     return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
 
-pat = []
-n = 5
-p1=np.array([[2,0],[0,0],[0,1],[4,1],
-            [4,2],[2,2],[2,1]
-            ])
+def DropPoleB(x, y, *, heiB=1, lenB=2):
+    poleB = np.array([[0, 0], [0, heiB], [lenB, heiB], [lenB, 0]], dtype=float)
+    poleB[:, 0] += x
+    poleB[:, 1] += y
+    return poleB
 
-for i in range(1):
-    #polygon = Polygon(np.random.rand(n, 2), True)
-    polygon = Polygon(p1)
-    pat.append(polygon)
+
+pat = []
+n = 100
+intervalX, intervalY = 20, 15
+lenA, heiA = 8, 4
+
+
+x0A, y0A = intervalX / 2 - lenA / 2, intervalY / 2 - heiA / 2
+targetA = np.array([[0, 0], [0, heiA], [lenA, heiA], [lenA, 0]], dtype=float)
+targetA[:, 0] += x0A
+targetA[:, 1] += y0A
+polygonA = Polygon(targetA, True)
+pat.append(polygonA)
+
+for i in range(n):
+    xpoleB, ypoleB = intervalX * np.random.rand(), intervalY * np.random.rand()
+    polygonB = Polygon(DropPoleB(xpoleB, ypoleB), True)
+    pat.append(polygonB)
+
 
 p = PatchCollection(pat, alpha=.8)
-colors = 100 * np.random.rand(len(pat))
+colors = 250 * np.random.rand(len(pat))
 p.set_array(np.array(colors))
 
 fig, ax = plt.subplots()
@@ -31,8 +46,9 @@ ax.add_collection(p)
 
 for poly in pat:
     pp = np.array(poly.get_xy())
-
     print(PolyArea(pp[:, 0], pp[:, 1]))
-ax.set_xlim([0,5])
-ax.set_ylim([0,5])
+
+
+ax.set_xlim([0, intervalX])
+ax.set_ylim([0, intervalY])
 plt.show()
