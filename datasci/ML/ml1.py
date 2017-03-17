ee669c28d5428441ef3f5b571178193160f215b6
bakeryproducts <gleb.m.sokolov>
2017-03-17 17:03:10 +0000

    ML

--- datasci/ML/ml1.py
+++ datasci/ML/ml1.py
@@ -1,8 +1,11 @@
-# 1 in ML, google stocks analysis
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import quandl
+import math
+from sklearn import preprocessing,svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 api_key= open('api.txt','r').read()
 quandl.ApiConfig.api_key=api_key
@@ -14,10 +17,29 @@ df.columns=['AO','AH','AL','AC','AV']
 
 df['HL_PCT'] = 100*(df['AH']-df['AL'])/df['AL']
 df['OC_PCT'] = 100*(df['AC']-df['AO'])/df['AO']
-
 df = df[['AC','HL_PCT','OC_PCT','AV']]
+#df.fillna(-99999,inplace=True)
 
-print(df['AV'].describe())
 
+forecast_col = 'AC'
+forecast_shift = int(math.ceil(len(df)*.01))
+df['Y'] = df[forecast_col].shift(-forecast_shift)
+df.dropna(inplace=True)
+
+X = np.array(df.drop('Y',axis=1))
+#print(df.drop(['AV','AC'],axis=1))
+
+y = np.array(df['Y'])
+
+X = preprocessing.scale(X)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.2)
+
+clf = svm.SVR()
+#clf = LinearRegression()
+clf.fit(X_train,y_train)
+acc = clf.score(X_test,y_test)
+
+print(acc)
 df.plot()
-plt.show()
+#plt.show()
